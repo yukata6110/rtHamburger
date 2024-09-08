b06b9ad7ea34464c480882b6391d35aecdafe05e
yukata <zeitungyuka@gmail.com>
2024-09-08 15:30:16 +0900

    20240908sidebar

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+//const btn = document.querySelector ('.p-sidebar__btnMenu');
+//const sidebarList = document.querySelector ('.js-sidebar');
+//btn.addEventListener ('click', ()=> {
+//sidebarList.classList.toggle('js-sidebar__open');
+//});
+
+
+//const btn = document.querySelector ('.p-sidebar__btnMenu');
+//const sidebarList = document.querySelector ('.js-sidebar');
+//btn.addEventListener ('click', ()=> {
+//
+//    if (sidebarList === '.js-sidebar__close' ) {
+//        sidebarList.classList.toggle('js-sidebar__open');
+//    } else {
+//        sidebarList.classList.toggle('js-sidebar__close');
+//    }
+//});
