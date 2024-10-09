173ce0a1e12e6bee226e194f131efd121af4a5ee
yukata <zeitungyuka@gmail.com>
2024-10-10 06:58:30 +0900

    20241010

--- js/main.js
+++ js/main.js
@@ -2,8 +2,29 @@
 const btn = document.querySelector ('.p-sidebar__btnMenu');
 const sidebarList = document.querySelector ('.js-sidebar');
 const sidebarListBg = document.querySelector ('.js-sidebar__bg');
+
+
+
 btn.addEventListener ('click', ()=> {
     btn.classList.toggle('js-btnMenu__open');
     sidebarList.classList.toggle('js-sidebar__open');
     sidebarListBg.classList.toggle('js-sidebar__openBg');
 });
+
+/*
+
+if (window.matchMedia('(min-width:1280px)').matches){
+    btn.removeEventListener ('click', ()=> {
+        btn.classList.toggle('js-btnMenu__open');
+        sidebarList.classList.toggle('js-sidebar__open');
+        sidebarListBg.classList.toggle('js-sidebar__openBg');
+    });
+} else {
+    btn.addEventListener ('click', ()=> {
+        btn.classList.toggle('js-btnMenu__open');
+        sidebarList.classList.toggle('js-sidebar__open');
+        sidebarListBg.classList.toggle('js-sidebar__openBg');
+    });
+
+}
+*/
