aef193d497fbc9980b6f63b83eed92a1214768d3
yukata <zeitungyuka@gmail.com>
2024-11-19 06:47:40 +0900

    20241119

--- js/main.js
+++ js/main.js
@@ -1,30 +1,17 @@
 
-const btn = document.querySelector ('.p-sidebar__btnMenu');
+const btn = document.querySelector ('.js-btn-menu');
 const sidebarList = document.querySelector ('.js-sidebar');
 const sidebarListBg = document.querySelector ('.js-sidebar__bg');
 
-
-
 btn.addEventListener ('click', ()=> {
-    btn.classList.toggle('js-btnMenu__open');
+    btn.classList.toggle('js-btn-menu__open');
     sidebarList.classList.toggle('js-sidebar__open');
     sidebarListBg.classList.toggle('js-sidebar__openBg');
 });
 
-/*
+window.addEventListener('resize', ()=>{
+    btn.classList.remove('js-btn-menu__open');
+    sidebarList.classList.remove('js-sidebar__open');
+    sidebarListBg.classList.remove('js-sidebar__openBg');
+})
 
-if (window.matchMedia('(min-width:1280px)').matches){
-    btn.removeEventListener ('click', ()=> {
-        btn.classList.toggle('js-btnMenu__open');
-        sidebarList.classList.toggle('js-sidebar__open');
-        sidebarListBg.classList.toggle('js-sidebar__openBg');
-    });
-} else {
-    btn.addEventListener ('click', ()=> {
-        btn.classList.toggle('js-btnMenu__open');
-        sidebarList.classList.toggle('js-sidebar__open');
-        sidebarListBg.classList.toggle('js-sidebar__openBg');
-    });
-
-}
-*/
