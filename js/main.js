d05aa3b900d256cd55a4198f5c33407e474e26e4
yukata <zeitungyuka@gmail.com>
2024-12-26 07:02:27 +0900

    20241226

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@
 const btn = document.querySelector ('.js-btn-menu');
 const sidebarList = document.querySelector ('.js-sidebar');
 const sidebarListBg = document.querySelector ('.js-sidebar-bg');
+const body = document.body;
 
 btn.addEventListener ('click', ()=> {
     btn.classList.toggle('js-btn-menu-open');
     sidebarList.classList.toggle('js-sidebar-open');
     sidebarListBg.classList.toggle('js-sidebar-openBg');
+    body.classList.toggle('js-body-scroll');
 });
+
+
 
 window.addEventListener('resize', ()=>{
     btn.classList.remove('js-btn-menu-open');
